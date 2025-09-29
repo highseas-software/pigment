afa3474edf666d62811651e9ff0ca4e93684c942
p <38130262+scallywaag@users.noreply.github.com>
2025-09-29 22:06:39 +0300

    add some examples

---
+++ example/example.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/highseas-software/pigment"
+)
+
+func main() {
+	// Base colors and modifiers
+	fmt.Println(pigment.Red("this is red text"))
+	fmt.Println(pigment.Bold("this is bold text"))
+
+	// Multiple strings
+	fmt.Println(pigment.Red("one", "two", "three"))
+
+	// Nesting
+	fmt.Println(
+		pigment.Red("this is red.. " +
+			pigment.Bold("now also bold.. ") +
+			"and now just red again."),
+	)
+
+	// Chaining
+	fmt.Println(pigment.WithBold().Red("this is red and bold"))
+}
