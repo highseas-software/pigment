be3d8026dea1df58daf519e51b23f78a7fc0374e
scallywaag <38130262+scallywaag@users.noreply.github.com>
2025-10-03 22:16:35 +0300

    fix reset for WithBg

--- colors.go
+++ colors.go
@@ -6,6 +6,7 @@ func PrintColors() {
 	for i := range 256 {
 		fmt.Printf("\x1b[48;5;%dm  \x1b[0m", i)
 		fmt.Printf("\x1b[38;5;%dm%03d\x1b[0m ", i, i)
+
 		if (i % 6) == 3 {
 			fmt.Println()
 		}
@@ -24,7 +25,7 @@ func WithFG(colorID uint8) style {
 
 func (c *composer) WithBG(colorID uint8) style {
 	code := fmt.Sprintf("\x1b[48;5;%dm", colorID)
-	reset := fmt.Sprintf("\x1b[%dm", 39)
+	reset := fmt.Sprintf("\x1b[%dm", 49)
 	return createComposer(c, code, reset)
 }
 
