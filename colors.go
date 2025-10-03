f655c6de8539c00edd9d4f0b875f80f15a472fce
scallywaag <38130262+scallywaag@users.noreply.github.com>
2025-10-03 21:44:33 +0300

    move PrintColors to top, remove description log

--- colors.go
+++ colors.go
@@ -3,7 +3,6 @@ package pigment
 import "fmt"
 
 func PrintColors() {
-	fmt.Println("ANSI 256 Color Palette:")
 	for i := range 256 {
 		fmt.Printf("\x1b[48;5;%dm  \x1b[0m", i)
 		fmt.Printf("\x1b[38;5;%dm%03d\x1b[0m ", i, i)
--- example/example.go
+++ example/example.go
@@ -7,6 +7,9 @@ import (
 )
 
 func main() {
+	// 256 ANSI Colors
+	pigment.PrintColors()
+
 	// Base colors and modifiers
 	fmt.Println(pigment.Red("this is red text"))
 	fmt.Println(pigment.Bold("this is bold text"))
@@ -23,9 +26,6 @@ func main() {
 
 	// Chaining
 	fmt.Println(pigment.WithBold().Red("this is red and bold"))
-
-	// 256 ANSI Colors
-	pigment.PrintColors()
 
 	// ANSI 256 FG and BG
 	fmt.Println(pigment.WithFG(111).String("this is an ansi foreground"))
